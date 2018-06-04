65981b0b1a5f80192e05d8092c1d6c6f02babfe8
Benjamin Maurer <maurer.benjamin@gmail.com>
2018-06-04 13:39:07 +0200

    Found the 'fix'.

--- src/main/java/com/example/demo/persistence/repository/FooRepository.java
+++ src/main/java/com/example/demo/persistence/repository/FooRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.persistence.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -12,7 +13,11 @@ import com.example.demo.persistence.model.Foo;
 @RestResource
 public interface FooRepository extends JpaRepository<Foo, Long> {
   
-  @EntityGraph(value = "Foo.full", type = EntityGraphType.LOAD)
+  @EntityGraph("Foo.full")
   @Override
   List<Foo> findAll();
+
+  @EntityGraph("Foo.full")
+  @Override
+  Page<Foo> findAll(Pageable pageable);
 }
